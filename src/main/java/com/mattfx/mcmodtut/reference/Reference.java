2ee2f16d64603e0dff0246a9bb5860a21c0503e5
Matt <matthiasvereecken@msn.com>
2014-07-24 21:05:43 +0200

    Add proxy reference strings

--- src/main/java/com/mattfx/mcmodtut/reference/Reference.java
+++ src/main/java/com/mattfx/mcmodtut/reference/Reference.java
@@ -5,4 +5,6 @@ public class Reference {
     public static final String MOD_ID = "MCModTut";
     public static final String MOD_NAME = "MC Mod Tut";
     public static final String VERSION = "1.7.2-1.0";
+    public static final String CLIENT_PROXY_CLASS = "com.mattfx.mcmodtut.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.mattfx.mcmodtut.proxy.ServerProxy";
 }
