7934ced19a06edbbc6798fa76438f6ad4f4b1132
Matt <matthiasvereecken@msn.com>
2014-07-24 21:00:03 +0200

    Removed unnecessary comments

--- src/main/java/com/mattfx/mcmodtut/proxy/ClientProxy.java
+++ src/main/java/com/mattfx/mcmodtut/proxy/ClientProxy.java
@@ -1,7 +1,5 @@
 package com.mattfx.mcmodtut.proxy;
 
-/**
- * Created by Matthias on 24/07/2014.
- */
+
 public class ClientProxy extends CommonProxy {
 }
--- src/main/java/com/mattfx/mcmodtut/reference/Reference.java
+++ src/main/java/com/mattfx/mcmodtut/reference/Reference.java
@@ -1,8 +1,6 @@
 package com.mattfx.mcmodtut.reference;
 
-/**
- * Created by Matthias on 24/07/2014.
- */
+
 public class Reference {
     public static final String MOD_ID = "MCModTut";
     public static final String MOD_NAME = "MC Mod Tut";
