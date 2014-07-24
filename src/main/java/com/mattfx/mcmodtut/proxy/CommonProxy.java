c72447fa8cdf099c4a0f2c8eea7b33bb4eae246f
MattFX <matthiasvereecken@msn.com>
2014-07-24 20:54:43 +0200

    Up to episode 5

---
+++ src/main/java/com/mattfx/mcmodtut/proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package com.mattfx.mcmodtut.proxy;
+
+/**
+ * Created by Matthias on 24/07/2014.
+ */
+public class CommonProxy implements IProxy {
+}
---
+++ src/main/java/com/mattfx/mcmodtut/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.mattfx.mcmodtut.reference;
+
+/**
+ * Created by Matthias on 24/07/2014.
+ */
+public class Reference {
+    public static final String MOD_ID = "MCModTut";
+    public static final String MOD_NAME = "MC Mod Tut";
+    public static final String VERSION = "1.7.2-1.0";
+}
